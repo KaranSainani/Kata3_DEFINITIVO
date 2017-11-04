88d175bbb82bfe054187cf7254cf2813f3a02a15
karansainani <karansainani@192.168.1.33>
2017-11-04 23:34:32 +0000

    Primera versión de Kata3

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,18 @@
+package kata3;
+
+/**
+ * class kata3 -  kata3_v1
+ * 
+ * @author karansainani
+ */
+
+
+public class Kata3 {
+
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+    
+}
